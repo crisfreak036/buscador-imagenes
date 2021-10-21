21a14b267f545eff68062359933e5c4fa0ac529a
crisfreak <cristopher.sotog@utem.cl>
2021-10-21 10:09:58 -0300

    Add a function to show alerts in the document

--- js/interfaz.js
+++ js/interfaz.js
@@ -1,5 +1,39 @@
 // Selectores
-const formulario = document.querySelector('#formulario');
-const inputTermino = document.querySelector('#termino');
-const contenedorResultado = document.querySelector('#resultado');
-const paginacion = document.querySelector('#paginacion');
+export  const formulario = document.querySelector('#formulario'),
+            inputTermino = document.querySelector('#termino'),
+            contenedorResultado = document.querySelector('#resultado'),
+            paginacion = document.querySelector('#paginacion');
+
+
+// Funciones
+
+export function mostrarAlerta(mensaje, tipo, tiempo){
+    if(!document.querySelector('.alerta-personalizada')){
+
+        // Crea un contenedor
+        const alerta = document.createElement('p');
+        
+        // Dependiendo el tipo, se agregan ciertas clases
+        if( tipo === 'error'){
+            alerta.classList.add('bg-red-100', 'border-red-400', 'text-red-700', 'px-4', 'py-3', 'rounded','max-w-lg', 'mx-auto','mt-6', 'text-center', 'alerta-personalizada');
+            // Agrega contenido al contenedor
+            alerta.innerHTML = `
+            <strong class="font-bold">Error!</strong>
+            <span class="block sm:inline">${mensaje}</span>
+            `
+        }else{
+            alerta.classList.add('bg-green-100', 'border-green-400', 'text-green-700', 'px-4', 'py-3', 'rounded','max-w-lg', 'mx-auto', 'mt-6', 'text-center', 'alerta-personalizada');
+            alerta.innerHTML = `
+            <span class="block sm:inline">${mensaje}</span>
+            `
+        }
+        
+        // Agrega la alerta al HTML
+        formulario.appendChild(alerta);
+        
+        // Elimina la alerta después de 2 segundos
+        setTimeout(() => {
+            alerta.remove();
+        }, tiempo);
+    }
+}
