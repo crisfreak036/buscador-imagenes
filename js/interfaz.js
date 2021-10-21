49b3da898d077cd5b44a364acff01738657a7634
crisfreak <cristopher.sotog@utem.cl>
2021-10-21 09:24:47 -0300

    Add new interfaz.js file to thew project

---
+++ js/interfaz.js
@@ -0,0 +1,5 @@
+// Selectores
+const formulario = document.querySelector('#formulario');
+const inputTermino = document.querySelector('#termino');
+const contenedorResultado = document.querySelector('#resultado');
+const paginacion = document.querySelector('#paginacion');
