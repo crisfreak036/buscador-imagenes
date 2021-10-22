57a834d58f9c44dfc1d2ee343cec28aa1173c274
crisfreak <cristopher.sotog@utem.cl>
2021-10-22 00:03:36 -0300

    Create new module for some functions

---
+++ js/funciones.js
@@ -0,0 +1,11 @@
+
+// Generador que va a registrar la cantidad de elementos de acuerdo a las páginas
+export function *crearPaginador(totalPaginas){
+    for(let i = 1; i <= totalPaginas; i++){
+        console.log(i);
+    }
+}
+
+export function calcularPaginas( total, registrosPorPagina ){
+    return parseInt( Math.ceil( total / registrosPorPagina ))
+}
