73b4b388b3f66351352366b5896526788ee4906a
crisfreak <cristopher.sotog@utem.cl>
2021-10-28 16:45:42 -0300

    Migrate the request to async-await

--- js/funciones.js
+++ js/funciones.js
@@ -16,17 +16,24 @@ export function validadFormulario(e){
     buscaImagenes();
 }
 
-export function buscaImagenes(){
+export async function buscaImagenes(){
     const termino = UI.inputTermino.value;
     const key = '23957148-599aa7fca85d3a65cfb381d7c';
     const url = `https://pixabay.com/api/?key=${key}&q=${termino}&per_page=${resultadosPorPagina}&page=${UI.paginaActual}`;
-    fetch(url)
-    .then( respuesta => respuesta.json() )
-    .then( resultados => {
-        const { hits, totalHits } = resultados;
-        totalPaginas = calcularPaginas( totalHits, resultadosPorPagina );
-        UI.mostrarImagenes(hits, totalPaginas);
-    } )
+
+    // Consulta con try-catch-async-await
+    try {
+        const respuesta = await fetch(url);
+        const resultados = await respuesta.json();
+        const destructuring = function(resultados){
+            const { hits, totalHits } = resultados;
+            totalPaginas = calcularPaginas( totalHits, resultadosPorPagina );
+            UI.mostrarImagenes(hits, totalPaginas);
+        }
+        destructuring(resultados);
+    } catch (error) {
+        console.log(error);
+    }
 }
 
 // Generador que va a registrar la cantidad de elementos de acuerdo a las páginas
